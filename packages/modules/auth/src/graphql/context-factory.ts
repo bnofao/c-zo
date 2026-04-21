ab0b837071cfd0c815e8d90c590ceb0ddb393381
Fawaz AJANI <ajanifawaz@gmail.com>
2026-04-21 11:06:41 +0000

    refactor(auth/graphql): rewrite buildAuthContext for new service shape

--- packages/modules/auth/src/graphql/context-factory.ts
+++ packages/modules/auth/src/graphql/context-factory.ts
@@ -1,24 +1,49 @@
 import { registerContextFactory } from '@czo/kit/graphql'
 import { useContainer } from '@czo/kit/ioc'
-import '../types'
+import type { AuthContext } from '../types'
 
-registerContextFactory('auth', async (serverCtx) => {
+export async function buildAuthContext(request: Request): Promise<AuthContext> {
   const container = useContainer()
 
-  const request = serverCtx.request as Request
-  const authService = await container.make('auth:service')
-  const authSession = await authService.getSession(request.headers)
+  const [
+    userService,
+    organizationService,
+    accountService,
+    sessionService,
+    twoFactorService,
+    apiKeyService,
+    appService,
+    authService,
+  ] = await Promise.all([
+    container.make('auth:users'),
+    container.make('auth:organizations'),
+    container.make('auth:accounts'),
+    container.make('auth:sessions'),
+    container.make('auth:twoFactor'),
+    container.make('auth:apikeys'),
+    container.make('auth:apps'),
+    container.make('auth:service'),
+  ])
+
+  const session = await authService.getSession(request.headers)
 
   return {
-    auth: {
-      instance: await container.make('auth'),
-      userService: await container.make('auth:users'),
-      organizationService: await container.make('auth:organizations'),
-      authService,
-      apiKeyService: await container.make('auth:apikeys'),
-      appService: await container.make('auth:apps'),
-      session: authSession?.session ?? null,
-      user: authSession?.user ?? null,
-    },
+    userService,
+    organizationService,
+    accountService,
+    sessionService,
+    twoFactorService,
+    apiKeyService,
+    appService,
+    authService,
+    session: session ?? null,
+    user: session?.user ?? null,
   }
+}
+
+registerContextFactory('auth', async (serverCtx) => {
+  const request = serverCtx.request as Request
+  const ctx = await buildAuthContext(request)
+
+  return { auth: ctx }
 })
