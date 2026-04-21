d773bc7d0414c01dbec9aaf39840708a66ddcc55
Fawaz AJANI <ajanifawaz@gmail.com>
2026-04-21 09:20:07 +0000

    chore(kit/graphql): update public re-exports for Pothos migration

--- packages/kit/src/graphql/index.ts
+++ packages/kit/src/graphql/index.ts
@@ -1,5 +1,7 @@
 export * from './context'
 export * from './subscription'
 export * from './types'
-// NB: directive/middleware/relay/resolvers/schema ont été déplacés dans old/
-// Les nouveaux exports (builder, errors, scalars, sdl) seront ajoutés dans Task 7.1
+export * from './builder'
+export * from './sdl'
+export * from './errors'
+export * from './scalars'
